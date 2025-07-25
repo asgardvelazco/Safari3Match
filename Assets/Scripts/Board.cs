63d5d699a1b34a754a9b6eed71759b4760a09619
Asgard Velazco <137465951+asgardvelazco@users.noreply.github.com>
2025-07-24 22:37:36 -0600

    Coordinates
    Cordinetes assignation for each tile

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -33,6 +33,7 @@ public class Board : MonoBehaviour
         Camera.main.orthographicSize = horizontal > vertical ? horizontal + cameraSizeOffset : vertical + cameraSizeOffset;
 
     }
+
     private void SetupBoard()
     {
         for (int x = 0; x < width; x++)
@@ -41,6 +42,7 @@ public class Board : MonoBehaviour
             {
                 var o = Instantiate(tileObject, new Vector3(x, y, -5), Quaternion.identity);
                 o.transform.parent = transform;
+                o.GetComponent<Tile>()?.Setup(x, y, this);
             }
         }
     }
