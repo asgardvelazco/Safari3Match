78a790a427e030f1b1395d32738be60f9fddd2d3
Asgard Velazco <137465951+asgardvelazco@users.noreply.github.com>
2025-10-29 21:21:38 -0600

    UIInGameLogic

---
+++ Assets/Scripts/UIInGame.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class UIInGame : MonoBehaviour
+{
+    public void PauseButtonPressed()
+    {
+        //pausar el juego
+    }
+}
