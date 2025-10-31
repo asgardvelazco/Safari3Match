42de670ebafbd078349d7223175af7a67f2ac2df
Asgard Velazco <137465951+asgardvelazco@users.noreply.github.com>
2025-10-30 21:38:20 -0600

    UIPauseScreenLogic

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -67,6 +67,26 @@ public class Board : MonoBehaviour
         {
             ClearAllPieces();
         }
+        if (newState == GameManager.GameState.Idle)
+        {
+            ResetBoard();
+            ResetCamera();
+            setupMade = false;
+        }
+    }
+
+    private void ResetCamera()
+    {
+        Camera.main.orthographicSize = 5;
+        Camera.main.transform.position = new Vector3(0,0,-10);
+    }
+
+    private void ResetBoard()
+    {
+        foreach (Transform item in transform)
+        {
+            Destroy(item.gameObject);
+        }
     }
 
     private IEnumerator SetupPieces()
